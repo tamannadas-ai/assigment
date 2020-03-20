43104d6e132af02649e32a20dce3bd77865ab744
tamannadas-ai <62437657+tamannadas-ai@users.noreply.github.com>
2020-03-20 22:44:34 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b;
+    int s,p,d,div;
+    scanf("%d%d",&a,&b);
+    s=a+b;
+    d=a-b;
+    p=a*b;
+    div=a/b;
+    printf("Sum of a and b is %d\n",s);
+          printf("Difference of a and b is %d\n",d);
+
+          printf("Product of a and b is %d\n",p);
+          printf("Division of a and b is %d\n",div);
+return 0;
+}
